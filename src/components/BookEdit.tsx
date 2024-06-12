a3a96794bb6f03f794adadadf6361ac03ab85bf7
Santiago Cano <sanblackx1@gmail.com>
2024-06-12 14:31:07 -0400

    ADD: Toggle Form for adding elements

--- src/components/BookEdit.tsx
+++ src/components/BookEdit.tsx
@@ -1,18 +1,44 @@
-import { PencilSquareIcon } from '@heroicons/react/24/outline';
+import { PencilSquareIcon, PlusCircleIcon } from '@heroicons/react/24/outline';
 import style from './BookShow.module.css';
+import { useState } from 'react';
 type props = {
   title: string;
   onEditBook: () => void;
 };
 export default function BookEdit({ title, onEditBook }: props) {
-  return (
-    <div className={style.card__content__edit}>
+  const [showEdit, setShowEdit] = useState(false);
+  const [editTitle, setEditTitle] = useState(title);
+
+  const onClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setShowEdit((prev) => !prev);
+  };
+
+  const onChangeInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditTitle(event.target.value);
+  };
+
+  let content = (
+    <>
       <p title={title} aria-label={title}>
         {title}
       </p>
-      <button className={style.card__btn} type='button'>
+      <button className={style.card__btn} type='button' onClick={onClickHandler}>
         <PencilSquareIcon width={25} />
       </button>
-    </div>
+    </>
   );
+
+  if (showEdit) {
+    content = (
+      <form action='' method='post' className={style.card__content__edit}>
+        <label htmlFor='title'>
+          <input type='text' name='title' value={editTitle} onChange={onChangeInputHandler} />
+        </label>
+        <button className={style.card__btn} type='submit' onClick={onClickHandler}>
+          <PlusCircleIcon width={25} />
+        </button>
+      </form>
+    );
+  }
+  return <div className={style.card__content__edit}>{content}</div>;
 }
--- src/components/NewBook.tsx
+++ src/components/NewBook.tsx
@@ -4,8 +4,10 @@ import { Book } from '../types';
 
 type props = {
   addBook: (title: string) => void;
+  handleRef: React.LegacyRef<HTMLFormElement>;
+  toggle: (value: boolean) => void;
 };
-export const NewBook = ({ addBook }: props) => {
+export const NewBook = ({ addBook, handleRef, toggle }: props) => {
   const [title, setTitle] = useState('');
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
@@ -15,14 +17,21 @@ export const NewBook = ({ addBook }: props) => {
     e.preventDefault();
     addBook(title);
     setTitle('');
+    toggle(false);
   };
   return (
-    <form method='post' className={moduleCss.form__content} onSubmit={handleSubmit}>
+    <form
+      method='post'
+      className={moduleCss.form__content}
+      ref={handleRef}
+      onSubmit={handleSubmit}
+    >
       <h2 style={{ marginBottom: '10px' }}>Add a book</h2>
       <label htmlFor='titleBook'>Title</label>
       <input
         type='text'
         name='titleBook'
+        required
         id='titleBook'
         value={title} // Uncontrolled component {Please resolved}
         onChange={handleChange}
