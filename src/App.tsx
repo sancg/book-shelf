34ee6630dbbe71fc06d084e22a49f1262b888b56
Santiago Cano <sanblackx1@gmail.com>
2024-06-26 14:57:35 -0400

    ADD: Data persistance with API Requests

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,62 @@ function App() {
   const [showForm, setShowForm] = useState(false);
   const newBookForm = useRef<HTMLFormElement>(null);
 
-  const [books, setBooks] = useState([
-    { title: 'Harry Potter', id: 19235 },
-    { title: 'The foundation trilogy and beyond', id: 1451023 },
-  ]);
+  const [books, setBooks] = useState<Book[]>([]);
 
-  const createBook = (title: string) => {
-    const newBook = { title, id: Math.random() + new Date().getTime() };
+  const getAllBooks = async () => {
+    const books = await fetch('http://127.0.0.1:3001/books').then((r) => r.json());
+    setBooks(books);
+  };
+
+  useEffect(() => {
+    getAllBooks();
+  }, []);
+
+  useEffect(() => {}, [books]);
+
+  const createBook = async (title: string) => {
+    const newBook: Book = await fetch('http://127.0.0.1:3001/books', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({ title: title?.trim() }),
+    }).then((r) => r.json());
+
     setBooks((prev) => [...prev, newBook]);
   };
 
-  const editBook = (updateBook: Book) => {
+  const editBook = async (updateBook: Book) => {
+    const { title } = updateBook;
+    const changeBook = await fetch(`http://localhost:3001/books/${updateBook.id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PUT',
+      body: JSON.stringify({ title: title?.trim() }),
+    }).then((r) => r.json());
+
     const updateBooks: Book[] = books.map((book) => {
-      if (book.id === updateBook.id) {
-        return updateBook;
+      if (book.id === changeBook.id) {
+        return changeBook;
       }
       return book;
     });
-    console.log({ books, updateBooks });
+    // console.log({ books, updateBooks });
     setBooks(updateBooks);
   };
 
-  const deleteBook = (remove: Book) => {
-    const filtered = books.filter((book) => book.id !== remove.id);
-    setBooks(filtered);
+  const deleteBook = async (remove: Book) => {
+    const removeBook = await fetch(`http://localhost:3001/books/${remove.id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'DELETE',
+    }).then((r) => r.status);
+    if (removeBook === 200) {
+      const filtered = books.filter((book) => book.id !== remove.id);
+      setBooks(filtered);
+    }
   };
 
   const toggleNewBookForm = (value: boolean) => setShowForm(value);
